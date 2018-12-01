1602ea9f030d6999bc9f8f3f17e0b8f408e85e58
amohit <amohit@juniper.net>
2018-12-01 23:32:46 +0530

    added html files

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,14 @@ from django.conf.urls  import *
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^customer/', views.view_customer, name='view_customer')
+    url(r'^customer/', views.view_customer, name='view_customer'),
+    url(r'^view_credit_notes/', views.view_credit_notes, name='view_credit_notes'),
+    url(r'^view_delivery_notes/', views.view_delivery_notes, name='view_delivery_notes'),
+    url(r'^view_invoices/', views.view_invoices, name='view_invoices'),
+    url(r'^view_preference/', views.view_preference, name='view_preference'),
+    url(r'^view_product/', views.view_product, name='view_product'),
+    url(r'^view_purchase_order/', views.view_purchase_order, name='view_purchase_order'),
+    url(r'^view_quotes/', views.view_quotes, name='view_quotes'),
+    url(r'^view_report/', views.view_report, name='view_report')
+
 ]
