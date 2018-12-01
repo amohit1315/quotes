7856db6e663f53130b7d0c81c7a4a8bf4d52bacb
ESHAN GULHATI <eshan.gulhati@gmail.com>
2018-12-01 21:40:25 +0530

    Added Products and Service Models

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import Contact,Customer
+from models import Contact,Customer,Product,Service
 
 admin.site.register(Customer)
 admin.site.register(Contact)
