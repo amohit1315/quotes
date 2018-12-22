165509d6a089154ba6b037e90579059d10074f38
ESHAN GULHATI <eshan.gulhati@gmail.com>
2018-12-22 19:28:03 +0530

    Export Customer and Product Feature

--- quotes/settings.py
+++ quotes/settings.py
@@ -126,6 +126,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
  os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'eshan.gulhati@gmail.com'
